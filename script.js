63549128505736fbdd9e74f2130058fea7b1df01
Esteban Xelhuantzi <a324249@uach.mx>
2020-09-24 10:29:53 -0600

    agregado

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeImage(){
+    foco=document.getElementById('foco');
+    
+  //  alert(foco.src);
+
+    if(foco.src.match('focoOff')){
+        foco.src="focoOn.png"
+    }else{
+        foco.src="focoOff.png"
+    }
+
+}
